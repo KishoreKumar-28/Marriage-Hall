c52890f1929a8ef570b08621b33fc2fc22fc36c9
KishoreKumar-28 <126554513+KishoreKumar-28@users.noreply.github.com>
2023-03-16 20:51:08 +0530

    Add files via upload

---
+++ js/JothiMahal.js
@@ -0,0 +1,21 @@
+// var div=document.createElement("div")
+// var popup=document.getElementById("carouselExampleCaptions")
+// document.popup.append(div)
+
+// var h2=document.createElement("h2")
+// document.div.append(h2)
+// var h3=document.createElement("h3")
+// document.div.append(h3)
+// var h33=document.createElement("h3")
+// document.div.append(h33)
+// div.style.border="1px solid black"
+// div.style.height="200px"
+// div.style.position="fixed"
+// div.style.backgroundColor="red"
+document.getElementById("submit1").addEventListener("click",
+function submit()
+{
+    document.getElementById("submit").style.display="block"
+    document.getElementById("submit").style.position="relative"
+    document.getElementById("form").style.display="none" 
+})
